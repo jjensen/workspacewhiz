e49144472792f8b0b1d4bc9abfc594d6352c84f1
Joshua Jensen <jjensen@workspacewhiz.com>
2009-08-05 01:09:58 -0600

    * Remove old batch files.

--- Src/StartVC6.bat
+++
@@ -1,4 +0,0 @@
-set rp="\machine\software\Microsoft\VisualStudio\6.0\Setup\VsCommonDir"
-for /f "tokens=1*" %%i in ('dtreg -lv %rp%') do set VSDir=%%j
-start /D"%VSDIR%\MSDev98\Bin" msdev.exe %*
-
--- Src/StartVSNET.bat
+++
@@ -1,4 +0,0 @@
-set rp="\machine\software\Microsoft\VisualStudio\7.0\InstallDir"
-for /f "tokens=1*" %%i in ('dtreg -lv %rp%') do set VSDir=%%j
-start /D"%VSDIR%" devenv.exe %*
-
